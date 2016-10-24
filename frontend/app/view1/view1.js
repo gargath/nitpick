674e6b8103544f33cead7f14c81534615478c34e
Piter <piter@lightweaver.info>
2016-10-24 19:51:25 +0100

    Use angular's strict dependency injection

--- frontend/app/view1/view1.js
+++ frontend/app/view1/view1.js
@@ -1,14 +1,12 @@
 'use strict';
 
 angular.module('myApp.view1', ['ngRoute', 'ui.bootstrap', 'restangular'])
-  .controller('View1Ctrl', function ($scope, Restangular) {
+  .controller('View1Ctrl', ['$scope', '$http', '$q', 'Restangular', 'loginModal', function ($scope, $http, $q, Restangular, loginModal) {
     var baseUsers = Restangular.all('users');
-    $scope.foo = 'bar';
     baseUsers.getList().then(function (users) {
       $scope.allUsers = users;
     });
-
-  }).config(['$routeProvider', function ($routeProvider) {
+  }]).config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
