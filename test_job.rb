16046b9341875683d65477a7355de2c1e928947b
gargath <signup@lightweaver.info>
2016-10-28 13:55:08 +0100

    Resque now with logging in worker

---
+++ test_job.rb
@@ -0,0 +1,10 @@
+class TestJob
+ @queue = :test_queue
+
+ def self.perform
+  Resque.logger = Logger.new(STDOUT)
+  Resque.logger.level = Logger::DEBUG
+  Resque.logger.info "Job done!"
+ end
+end
+
