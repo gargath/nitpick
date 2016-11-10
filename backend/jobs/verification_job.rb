ea419f314865a874c01b364a23b1cee19d4072fe
gargath <signup@lightweaver.info>
2016-11-10 15:04:24 +0000

    Integrate Postmark pt 2

--- backend/jobs/verification_job.rb
+++ backend/jobs/verification_job.rb
@@ -27,9 +27,9 @@ MESSAGE_END
 
     mail_uri = URI.parse ENV['SMTP_URL']
 
-    Resque.logger.info "SMTP details:"
-    Resque.logger.info "Host: #{mail_uri.host}:#{mail_uri.port}"
-    Resque.logger.info "User: #{mail_uri.userinfo}"
+    @logger.info "SMTP details:"
+    @logger.info "Host: #{mail_uri.host}:#{mail_uri.port}"
+    @logger.info "User: #{mail_uri.userinfo}"
 
     if mail_uri.userinfo
       Net::SMTP.start(mail_uri.host, mail_uri.port, 'nitpick-6828.herokuapp.com', mail_uri.userinfo.split(':')[0], mail_uri.userinfo.split(':')[1]) do |smtp|
@@ -40,6 +40,6 @@ MESSAGE_END
         smtp.send_message message, 'nitpick@example.com', email
       end
     end
-    Resque.logger.info "Doing work for #{username} <#{email}>, token: #{token}, id: #{user_id}"
+    @logger.info "Doing work for #{username} <#{email}>, token: #{token}, id: #{user_id}"
   end
 end
